030fff2e3d470c9ebef49e6139655014152c0665
Viktoriia Prozorova <vika.prozorova@gmail.com>
2025-08-26 15:53:43 +0200

    Fix Header constructor

--- poms/components/Header.ts
+++ poms/components/Header.ts
@@ -1,21 +1,30 @@
-import { expect } from "@playwright/test";
 import { BaseComponent } from "./BaseComponent";
+import { BrowserContext, Page, Locator, expect } from "@playwright/test";
 
 export class Header extends BaseComponent {
-  selectors = {
-    myProfileMenu: this.page.locator('[id="userNavDropdown"]'),
-    logoutButton: this.page.getByRole("button", { name: "Logout" }),
-    signInButton: this.page.getByRole("button", { name: "Sign In" }),
-  };
+  selectors: Record<string, Locator>;
+
+  constructor(page: Page, context: BrowserContext) {
+    super(page, context);
+    this.selectors = {
+      myProfileMenu: this.page.locator('[id="userNavDropdown"]'),
+      logoutButton: this.page.getByRole("button", { name: "Logout" }),
+      signInButton: this.page.getByRole("button", { name: "Sign In" }),
+    };
+  }
+
   async isMyProfileMenuVisible() {
     await expect(this.selectors.myProfileMenu).toBeVisible();
   }
+
   async openMyProfileMenu() {
     await this.selectors.myProfileMenu.click();
   }
+
   async clickLogout() {
     await this.selectors.logoutButton.click();
   }
+
   async isSignInVisible() {
     await expect(this.selectors.signInButton).toBeVisible();
   }
